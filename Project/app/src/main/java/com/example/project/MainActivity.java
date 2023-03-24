2fb931fecc22e1f2b4185070aed77a5a92d08fae
Daniil <bans1111111111@gmail.com>
2023-03-24 22:04:14 +0300

    4

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.ImageFormat;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyApp";
+    TextView textView;
+
+    ImageView imageView;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+        textView.setText(R.string.muz);
+        imageView.setImageResource(R.drawable.mus);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick: ");
+            }
+        });
+    }
+
+    private void initUI(){
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
     }
 }
