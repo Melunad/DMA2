1d024e113c16d8736ebc2ae8e273843e66f72e0f
Daniil <bans1111111111@gmail.com>
2023-03-25 20:58:41 +0300

    переделал пункт 5 с возвращением данных

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyApp";
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initUI();
-        /*button.setOnClickListener(new View.OnClickListener() {
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i(TAG, "onClick: ");
+
                 Intent i = new Intent(MainActivity.this,MainActivity2.class);
-                startActivity(i);
+                startActivityForResult(i,1);
             }
-        });*/
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        textView.setText("RETURN DATA");
+
     }
 
     private void initUI(){
+        textView = findViewById(R.id.textView3);
         button = findViewById(R.id.button);
     }
 
-    public void Onclick(View view){
-        Log.i(TAG, "onClick: ");
-    }
 }
